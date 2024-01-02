5b341a753144327940ef2212d40040a4648a0958
Daksh Patel <daksh21697@gmail.com>
2024-01-02 14:04:22 -0800

    Improved readablity

--- a4_base.h
+++ a4_base.h
@@ -1,5 +1,3 @@
-// a4_base.h
-
 #pragma once
 
 #include <iostream>
@@ -8,19 +6,17 @@
 
 using namespace std;
 
-//
-// For conveniently printing vectors.
-//
+// Operator for printing vectors.
 template <typename T>
-ostream &operator<<(ostream &os, const vector<T> &v)
-{
+ostream &operator<<(ostream &os, const vector<T> &v){
+    
     int n = v.size();
     os << "{";
-    for (int i = 0; i < v.size(); i++)
-    {
+    for (int i = 0; i < v.size(); i++){
+
         os << v[i];
-        if (i < n - 1)
-        {
+        if (i < n - 1){
+
             os << ", ";
         }
     }
@@ -28,31 +24,28 @@ ostream &operator<<(ostream &os, const vector<T> &v)
     return os;
 }
 
-//
-// SortStats stores statistics about one run of a sorting routine.
-//
-
-// "ulong" is easier to type than "unsigned long"
+/*
+ SortStats stores statistics about one run of a sorting routine.
+ "ulong" is used to shorten "unsigned long"
+*/
 typedef unsigned long ulong;
 
-struct SortStats
-{
+struct SortStats{
+
     string sort_name;
     size_t vector_size = 0;
     ulong num_comparisons = 0;
     double cpu_running_time_sec = 0.0;
 
-    string to_csv() const
-    {
+    string to_csv() const{
+
         return sort_name + ", " + to_string(vector_size) + ", " + to_string(num_comparisons) + ", " + to_string(cpu_running_time_sec);
     }
-}; // struct SortStats
+};
 
-//
-// For conveniently printing SortStats objects.
-//
-ostream &operator<<(ostream &os, const SortStats &ss)
-{
+// Operator for printing SortStats objects.
+ostream &operator<<(ostream &os, const SortStats &ss){
+
     os << "SortStats{" << ss.sort_name
        << ", size=" << ss.vector_size
        << ", num_comparisons=" << ss.num_comparisons
@@ -61,19 +54,17 @@ ostream &operator<<(ostream &os, const SortStats &ss)
     return os;
 }
 
-//
-// Returns true if v[0] <= v[1] <= ... <= v[n-1], and false otherwise.
-// Returns true if v is empty, i.e. an empty vector is considered sorted.
-//
+/*
+ Returns true if v[0] <= v[1] <= ... <= v[n-1], and false otherwise.
+ Returns true if v is empty, i.e. an empty vector is considered sorted.
+*/
 template <typename T>
 bool is_sorted(vector<T> &v);
 
-//
-// Sorting algorithm headers. Each algorithm implements the named algorithm as
-// described in the textbook (iquick sort is described in the assignment), and
-// returns a SortStats object containing statistics about the run.
-//
-
+/*
+ Sorting algorithm headers. Each algorithm implements the named algorithm, and
+ returns a SortStats object containing statistics about the sorting run.
+*/
 template <typename T>
 SortStats bubble_sort(vector<T> &v);
 
@@ -95,10 +86,5 @@ SortStats shell_sort(vector<T> &v);
 template <typename T>
 SortStats iquick_sort(vector<T> &v);
 
-// See description in assignment.
-
-
-//
 // Returns a vector of n randomly chosen ints, each <= max and >= min.
-//
 vector<int> rand_vec(int n, int min, int max);
